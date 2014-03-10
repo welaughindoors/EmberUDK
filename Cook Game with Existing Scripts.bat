857e5785dc38c4f5ca68ae71e4e2f0933e7718a8
Inathero <inaprogs@gmail.com>
2014-03-09 22:15:48 -0600

    Cook Game Testing part 2
    Forgot actual script

---
+++ Cook Game with Existing Scripts.bat
@@ -0,0 +1,18 @@
+call Binaries\Win32\UDK.com CookPackages -full -platform=PC -nullrhi
+if errorlevel 1 goto error
+
+call Binaries\UnSetup.exe -GameCreateManifest
+if errorlevel 1 goto error
+
+call Binaries\UnSetup.exe -BuildGameInstaller
+if errorlevel 1 goto error
+
+:end
+
+echo Build successful.
+exit \b
+
+:error
+
+echo Build error!
+exit \b 1
